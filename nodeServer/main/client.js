85691d2f2dff13392af561029f8c4759410bbed1
Ubuntu <ubuntu@ip-172-31-38-232.ap-northeast-2.compute.internal>
2020-07-26 12:50:06 +0000

    20200726-2

--- nodeServer/main/client.js
+++ nodeServer/main/client.js
@@ -1,34 +1,27 @@
-window.onload = function(){ 
-    //클라이언트 소켓 생성
-    var socket = io.connect('ws://127.0.0.1:3000');
-    //DOM 참조
-    var div = document.getElementById('message');
-    var txt = document.getElementById('txtChat');
-    //텍스트 박스에 포커스 주기 
-    txt.focus();
-    
-    //텍스트 박스에 이벤트 바인딩
-    txt.onkeydown = sendMessage.bind(this); 
-    function sendMessage(event){     
-     if(event.keyCode == 13){
-      //메세지 입력 여부 체크   
-      var message = event.target.value;
-      if(message){
-        //소켓서버 함수 호출  
-        socket.emit('serverReceiver', message);
-        //텍스트박스 초기화
-        txt.value = '';
-      }
-     }
-    };
-    
-    //클라이언트 receive 이벤트 함수(서버에서 호출할 이벤트)
-    socket.on('clientReceiver', function(data){  
-      //console.log('서버에서 전송:', data);   
-      //채팅창에 메세지 출력하기
-      var message = data.msg;
-      div.innerText += message + '\r\n';
-      //채팅창 스크롤바 내리기  
-      div.scrollTop = div.scrollHeight;   
-    });
-   };
+const express = require('express');
+const path = require('path');
+var request = require('request');
+
+
+const app = express();
+const PORT = process.env.PORT = 8000;
+
+app.use(express.static('main'));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+    next();
+});
+
+const logger = require('./main/middleware/logger.js');
+app.use(logger());
+
+app.get('/kennel/check', function(req,res){
+    res.send(1);
+});
+
+app.listen(PORT,"0.0.0.0",function(req,res){
+  console.log('Server is running at:',PORT);
+});
--- nodeServer/server.js
+++ nodeServer/server.js
@@ -1,64 +1,79 @@
 const express = require('express');
 const path = require('path');
+var request = require('request');
 
 const app = express();
 const PORT = process.env.PORT = 8000;
 
 app.use(express.static('main'));
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-       res.header("Access-Control-Allow-Headers", "X-Requested-With");
-       res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-       next();
-   });
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+    next();
+});
 
 const logger = require('./main/middleware/logger.js');
 app.use(logger());
 
 var html;
 var js;
+var kennelStatus = 0;
 const fs = require('fs');
 
-fs.readFile('./main/kakaoNew.html',"utf8",function(err,data){
-  if(err){
-      throw err;
-  }
+
+fs.readFile('./main/kakaoNew.html', "utf8", function (err, data) {
+    if (err) {
+        throw err;
+    }
     html = data;
 });
 
-fs.readFile('./main/kakaoNew.js',"utf8",function(err,data){
-  if(err){
-      throw err;
-  }
+fs.readFile('./main/kakaoNew.js', "utf8", function (err, data) {
+    if (err) {
+        throw err;
+    }
     js = data;
 });
 
 
-app.get('/server/hello/:os', function(req,res){
-    res.json({"msg":req.params.os});
-    
+app.get('/server/hello/:os', function (req, res) {
+    res.json({ "msg": req.params.os });
+});
+
+app.get('/kennel/check', function (req, res) { // 안되는거~!
+
+    var kennel = 0;
+    request('http://192.168.0.48:8000/kennel/check', function (error, response, body) {
+        if (!error && response.statusCode == 200 || 304) {
+            kennel = 1;
+            console.log(kennel);
+        }
+    });
+
+    res.json({ "msg": kennel });
+
+});
+
+app.get('/location/map', function (req, res) {
+
+    var kakaourl = "//dapi.kakao.com/v2/maps/sdk.js?appkey=aefdc433d657b3802f48149819d88496&libraries=services";
+
+    var msg = { 'html': html, 'js': js, 'src': kakaourl };
+    var jsonData = JSON.stringify(msg);
+    res.send(jsonData);
+    //res.sendFile(path.join(__dirname, 'main', 'kakaoNew.html'));
 });
 
 
-
-
-app.get('/location/map', function(req,res){
-  console.log(html);
-
-  var kakaourl = "//dapi.kakao.com/v2/maps/sdk.js?appkey=aefdc433d657b3802f48149819d88496&libraries=services";
-
-  var msg = {'html':html, 'js':js, 'src':kakaourl};
-  var jsonData = JSON.stringify(msg);
-  res.send(jsonData);
-  //res.sendFile(path.join(__dirname, 'main', 'kakaoNew.html'));
-});
-
-
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'main', 'main.html'));
 });
 
-app.listen(PORT,"0.0.0.0",function(req,res){
-  console.log('Server is running at:',PORT);
+app.listen(PORT, "0.0.0.0", function (req, res) {
+    console.log('Server is running at:', PORT);
 });
+
+
+
