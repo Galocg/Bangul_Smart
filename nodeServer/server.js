50f9c6cf449b218b2f347ba9f9a67dcda3d8f9dc
Ubuntu <ubuntu@ip-172-31-38-232.ap-northeast-2.compute.internal>
2020-08-03 06:20:06 +0000

    20200803

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
-var request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const httprequest = require('request');
+const bp = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT = 8000;
+const PORT = process.env.PORT = 3000;
+
+const ras_home = 'https://www.naver.com';
+const ras_kennel = 'https://www.naver.com';
 
 app.use(express.static('main'));
 
@@ -17,62 +23,69 @@ app.use(function (req, res, next) {
 const logger = require('./main/middleware/logger.js');
 app.use(logger());
 
-var html;
-var js;
-var kennelStatus = 0;
-const fs = require('fs');
-
-// html source without script tag
-fs.readFile('./main/kakaoNoScript.html', "utf8", function (err, data) {
-    if (err) {
-        throw err;
-    }
-    html = data;
-});
-
-fs.readFile('./main/kakaoNew.js', "utf8", function (err, data) {
-    if (err) {
-        throw err;
-    }
-    js = data;
-});
-
-
 app.get('/server/hello/:os', function (req, res) {
     res.json({ "msg": req.params.os });
 });
 
-app.get('/kennel/check', function (req, res) { // 안되는거~!
+app.get('/home/check',function(req,res){
 
-    var kennel = 0;
-    request('http://192.168.0.48:8000/kennel/check', function (error, response, body) {
-        if (!error && response.statusCode == 200 || 304) {
-            kennel = 1;
-            console.log(kennel);
-        }
+    var status;
+
+    httprequest(ras_home, function(error, response, body){
+    if(!error && response.statusCode == 200){
+        status = 1;
+    }
+    else{
+        status = 0;
+    }
+    var msg = {"status": status};
+    res.json(msg);
     });
 
-    res.json({ "msg": kennel });
+});
+
+app.get('/home/url',function(req,res){
+    res.json({'msg':ras_home});
+});
+
+
+app.get('/kennel/check',function(req,res){
+    var status;
+
+    httprequest(ras_kennel, function(error, response, body){
+    if(!error && response.statusCode == 200){
+        status = 1;
+    }
+    else{
+        status = 0;
+    }
+    var msg = {"status": status};
+    res.json(msg);
+    });
 
 });
+
+app.get('/kennel/url',function(req,res){
+    res.json({'msg':ras_kennel});
+});
+
+app.get('/parse/test',function(req,res){
+    var jb;
+    httprequest('https://www.naver.com', function(error, response, body){
+    var $ = cheerio.load(body);
+    jb = $('h1').html();
+    console.log(jb);
+    });
+    res.json({"t":jb});
+});
+
 app.get('/location/kakaoNew.js',function(req,res){
     res.sendFile(path.join(__dirname,'main','kakaoNew.js'));
-})
+});
 
 app.get('/location/map/test', function (req, res) {
     res.sendFile(path.join(__dirname, 'main', 'kakaoNew.html'));
 });
-
-app.get('/location/map', function (req, res) {
-
-    var kakaourl = "//dapi.kakao.com/v2/maps/sdk.js?appkey=aefdc433d657b3802f48149819d88496&libraries=services";
-
-    var msg = { 'html': html, 'js': js, 'src': kakaourl };
-    var jsonData = JSON.stringify(msg);
-    res.send(jsonData);
-    //res.sendFile(path.join(__dirname, 'main', 'kakaoNew.html'));
-});
-
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'main', 'main.html'));
@@ -82,5 +95,26 @@ app.listen(PORT, "0.0.0.0", function (req, res) {
     console.log('Server is running at:', PORT);
 });
 
+
+
+// app.get('/kennel/movie', function(req,res){
+//     res.writeHead(200,{"Content-Type":"text/html"}); 
+//      fs.createReadStream("./test.html").pipe(response);
+// });
+
+
+// app.get('/kennel/movie', function (req, res) { // 채운 수정
+//     var stream = fs.createReadStream('http://192.168.43.77:5000/video_feed');
+//     //2. 잘게 쪼개진 stream이 몇번 전송되는지 확인하는  count
+//     var count = 0;
+//     //3. 잘게 쪼개진 data를 전송 할 수 있으면 data 이벤트를 발생시킨다
+//     stream.on('data', function(data){
+//         count = count +1;
+//         console.log('data count =' + count);
+//         //3.1 data 이벤트 발생되면 해당 data를 클라이언트로 전송한다.
+//         res.write(data);
+//     });
+//     res.end();
+// });
 
 
