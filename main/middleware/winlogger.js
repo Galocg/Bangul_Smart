1812026e458244269da83684597bd49f23553a7e
Ubuntu <ubuntu@ip-172-31-34-237.ap-northeast-2.compute.internal>
2020-08-23 09:37:52 +0000

    winstonlogger

--- main/middleware/winlogger.js
+++ main/middleware/winlogger.js
@@ -1,55 +1,32 @@
 const winston = require('winston');
+const { format } = require('winston');
 
-function leadingZeros(n, digits) {
-    var zero = '';
-    n = n.toString();
-  
-    if (n.length < digits) {
-      for (i = 0; i < digits - n.length; i++)
-        zero += '0';
-    }
-    return zero + n;
-}
+const moment = require('moment');
 
-const data = function(){
-    var d = new Date();
-    var s =
-      leadingZeros(d.getFullYear(), 4) + '-' +
-      leadingZeros(d.getMonth() + 1, 2) + '-' +
-      leadingZeros(d.getDate(), 2) + ' ' +
-  
-      leadingZeros(d.getHours(), 2) + ':' +
-      leadingZeros(d.getMinutes(), 2) + ':' +
-      leadingZeros(d.getSeconds(), 2);
-  
-    return s;
-}
+require('moment-timezone');
+moment.tz.setDefault("Asia/Seoul");
 
-const specificdata = function(){
-    var d = new Date();
-    var s =
-      leadingZeros(d.getFullYear(), 4) +
-      leadingZeros(d.getMonth() + 1, 2) +
-      leadingZeros(d.getDate(), 2)
-    return s;
-}
+var partdata = moment().format('YYYY-MM-DD');
 
-var fi = './log/'+specificdata()+'.log';
+var fname = './log/'+partdata+'.log';
 
 const logger = winston.createLogger({
+    format: format.combine(
+        format.splat(),
+        format.simple()
+    ),
     transports: [
       new winston.transports.Console(),
-      new winston.transports.File({ filename: fi })
+      new winston.transports.File({ filename: fname })
     ]
 });
 
   
 
 var logging = () => (req, res, next) =>{
-    logger.log({
-        level: 'info',
-        message: `${data()} ${req.method} ${req.url} ${req.headers['x-forwarded-for'] ||  req.connection.remoteAddress}`
-    });
+    var data = moment().format('YYYY-MM-DD HH:mm:ss');
+    var infostr = `${data} ${req.method} ${req.url} ${req.headers['x-forwarded-for'] ||  req.connection.remoteAddress}`
+    logger.log('info', infostr);
     next();
 };
 
